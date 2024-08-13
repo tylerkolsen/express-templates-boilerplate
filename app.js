73bce45e5b63838918aef5b9c3d92ebc9f55451f
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-13 12:46:49 -0600

    added files from code along

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+})
 
 // Start the server
 app.listen(port, () => {
@@ -37,3 +41,77 @@ const COMPLIMENTS = [
   'smashing',
   'lovely',
 ];
+
+app.get('/', (req, res) => {
+  res.render('home.html')
+})
+
+app.get('/hello', (req, res) => {
+  res.send(`
+    <html>
+      <body>
+        <h2>Hello!</h2>
+        <a href="/">Back to Home</a>
+      </body>
+    </html>
+    `)
+})
+
+// Displays the form
+app.get('/form', (req, res) => {
+  res.render('form.html')
+})
+
+// Handles the form
+app.get('/welcome', (req, res) => {
+  const person = req.query.person;
+  res.send(`Welcome, ${person}!`);
+})
+
+// Displays the number-form
+app.get('/number-form', (req, res) => {
+  res.render('number-form.html')
+})
+
+app.post('/fav-number', (req, res) => {
+  const favNumber = req.body.favNumber;
+  res.send(`Your favorite number is ${favNumber}`)
+})
+
+app.get('/user/:username', (req, res) => {
+  const username = req.params.username
+  res.send(`Your username is: ${username}`)
+})
+
+// This displays the form
+app.get('/temple-demo', (req, res) => {
+  const date = new Date() // This sets the variable date to the current date through a function
+  const formattedDate = `${date.toDateString()} ${date.toLocaleTimeString()}`
+  // we take the date variable, which has been converted to a Date class, and apply
+  // methods that convert the Date (which is a strange computer conversion)
+  // and convert it to a human readable version
+
+  res.render('template-demo.html.njk', {
+    todaysDate: formattedDate
+  })
+})
+
+// This utilizes the data from the 'temple-demo' pages form
+app.get('/greet', (req, res) => {
+  // const person = req.query.person;
+  // const wantsCompliments = req.query.wantsCompliments
+  // It's easier to read what's happening above, but below we can just
+  // deconstruct the object we're receiving to get the variables
+  const {person, wantsCompliments} = req.query
+
+  const randomCompliments = lodash.sampleSize(COMPLIMENTS, 3)
+
+  res.render('greet.html.njk', {
+    name: person,
+    compliments: wantsCompliments ? randomCompliments : false
+  })
+})
+
+app.get('/inherit', (req, res) => {
+  res.render('inherit.html.njk')
+})
